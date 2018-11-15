b028fff827ff237f05e38cd76a3a0af7eeb02988
Sayo Wonoda <wonoda.sayo@gmail.com>
2018-11-15 12:40:37 +0900

    modified design, add vue

--- js/editor.js
+++ js/editor.js
@@ -1,96 +1,117 @@
-function emphasis(match, p1, p2){
+function emphasis(match, p1, p2) {
   var index = p1.lastIndexOf(p2);
-  if(index != -1　&& index + p2.length == p1.length)
-  {
+  if (index != -1 && index + p2.length == p1.length) {
     var cut_empStr = p1.slice(0, index);
     return cut_empStr + '<span class=\"emp \">' + p2 + '</span>';
   }
   return match;
 }
 
-function ruby(match, p1, p2)
-{
+function ruby(match, p1, p2) {
   return '<ruby>' + p1 + '<rt>' + p2 + '</rt></ruby>';
 }
 
-function strong(match, p1, p2)
-{
+function strong(match, p1, p2) {
   var index = p1.lastIndexOf(p2);
-  if(index != -1 && index + p2.length == p1.length)
-  {
+  if (index != -1 && index + p2.length == p1.length) {
     var cut_empStr = p1.slice(0, index);
     return cut_empStr + '<strong>' + p2 + '</strong>';
   }
   return match;
 }
 
-function title(match, p1, p2, string)
-{
+function title(match, p1, p2, string) {
   var index = p1.lastIndexOf(p2);
-  if(index != -1 && index + p2.length == p1.length)
-  {
+  if (index != -1 && index + p2.length == p1.length) {
     var cut_empStr = p1.slice(0, index);
     return cut_empStr + '<span class=\"' + string + '\">' + p2 + '</span>';
   }
   return match;
 }
 
-function primaryTitle(match, p1, p2)
-{
+function primaryTitle(match, p1, p2) {
   return title(match, p1, p2, "primaryTitle");
 }
 
-function middleTitle(match, p1, p2)
-{
+function middleTitle(match, p1, p2) {
   return title(match, p1, p2, "middleTitle");
 }
 
-function subTitle(match, p1, p2)
-{
+function subTitle(match, p1, p2) {
   return title(match, p1, p2, "subTitle");
 }
 
-function newline(match, p1, p2)
-{
-  console.log("input new line code");
-  return "<p>　</p>";
+function convert(str) {
+  console.log(str);
+  var html = str.replace(/\n/g, '　\n');
+  html = marked(html);
+  html = html.replace(/[｜](.+?)《(.+?)》/g, ruby);
+  html = html.replace(/(.+?)［＃「(.+?)」に傍点］/g, emphasis);
+  html = html.replace(/(.+?)［＃「(.+?)」は太字］/g, strong);
+  html = html.replace(/(.+?)［＃「(.+?)」は大見出し］/g, primaryTitle);
+  html = html.replace(/(.+?)［＃「(.+?)」は中見出し］/g, middleTitle);
+  html = html.replace(/(.+?)［＃「(.+?)」は小見出し］/g, subTitle);
+  html = html.replace(/\n/g, '　<br />');
+  console.log(html);
+  if (html == '') return '';
+  return $('#result').html(html);
 }
 
-$(function(){
-  marked.setOptions({breaks: true});
-  var previewWindow = null;
-  $('#editor').keyup(function(){
-    var src = $(this).val();
-    var html = marked(src);
 
-    html = html.replace(/[｜](.+?)《(.+?)》/g, ruby);
-    html = html.replace(/(.+?)［＃「(.+?)」に傍点］/g, emphasis);
-    html = html.replace(/(.+?)［＃「(.+?)」は太字］/g, strong);
-    html = html.replace(/(.+?)［＃「(.+?)」は大見出し］/g, primaryTitle);
-    html = html.replace(/(.+?)［＃「(.+?)」は中見出し］/g, middleTitle);
-    html = html.replace(/(.+?)［＃「(.+?)」は小見出し］/g, subTitle);
-    html = html.replace(/\n/g, newline);
-    $('#result').html(html);
+window.onload = function () {
+  new Vue({
+    el: '#main',
+    data: {
+      input: '',
+    },
+    filters: {
+      convert: convert,
+    },
+    methods: {
+      vertical: function (event) {
+        event.preventDefault();
+        console.log("click", $('#preview'));
+        $('#preview').toggleClass('vertical');
+      }
+    }
   });
+};
 
-  $('.contain').on('click', function(event){
-    event.preventDefault();
-    $(this).toggleClass('vertical');
-  });
+//  $(function(){
+//   marked.setOptions({breaks: true});
+//   var previewWindow = null;
+//   $('#editor').keyup(function(){
+//     var src = $(this).val();
+//     var html = marked(src);
 
-  $('textarea').garlic();
-  $('[rel=persist]').garlic();
+//     html = html.replace(/[｜](.+?)《(.+?)》/g, ruby);
+//     html = html.replace(/(.+?)［＃「(.+?)」に傍点］/g, emphasis);
+//     html = html.replace(/(.+?)［＃「(.+?)」は太字］/g, strong);
+//     html = html.replace(/(.+?)［＃「(.+?)」は大見出し］/g, primaryTitle);
+//     html = html.replace(/(.+?)［＃「(.+?)」は中見出し］/g, middleTitle);
+//     html = html.replace(/(.+?)［＃「(.+?)」は小見出し］/g, subTitle);
+//     html = html.replace(/\n/g, newline);
+//     $('#result').html(html);
+//   });
 
-  $('#toPDF').click(function(){
-    console.log("click");
-    //printJS('result', 'html');
-    previewWindow = window.open('preview.html', "preview");
-    var html = $('#result').html();
-    console.log(html);
-    // previewWindow.postMessage(html);
-    // var previewWindow_id = $(previewWindow.document).find('#preview_result');
-    // console.log(previewWindow_id.html());
-    // previewWindow_id.val(html);
-    // console.log(previewWindow_id.val());
-  });
-});
+//   $('.contain').on('click', function(event){
+//     event.preventDefault();
+//     $(this).toggleClass('vertical');
+//   });
+
+//   $('textarea').garlic();
+//   $('[rel=persist]').garlic();
+
+//   $('#toPDF').click(function(){
+//     console.log("click");
+//     //printJS('result', 'html');
+//     previewWindow = window.open('preview.html', "preview");
+//     var html = $('#result').html();
+//     console.log(html);
+//     // previewWindow.postMessage(html);
+//     // var previewWindow_id = $(previewWindow.document).find('#preview_result');
+//     // console.log(previewWindow_id.html());
+//     // previewWindow_id.val(html);
+//     // console.log(previewWindow_id.val());
+//   });
+// });
